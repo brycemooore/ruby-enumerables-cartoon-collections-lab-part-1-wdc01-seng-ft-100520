76c2ad5c9398fb4aa6e70593049897bddaca7bcb
Bryce Moore <brycemooore@gmail.com>
2020-09-21 23:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  
   array.each{ |element|
   puts "Hello #{element.capitalize}!"
   }
@@ -11,6 +12,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
   array.each_with_index { |element, index|
   puts "#{index}.#{element.capitalize}"
   }

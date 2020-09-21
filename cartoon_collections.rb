df3dff83be9fc9af118d09ad665c73b66d67aa0d
Bryce Moore <brycemooore@gmail.com>
2020-09-21 23:06:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index { |element, index|
-  puts "#{index}.#{element.capitalize}"
+  puts "#{index} #{element.capitalize}"
   }
 end
